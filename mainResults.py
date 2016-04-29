fad885669e98d26e117132e4caa59e87109e0061
luxiaolei <26442779@qq.com>
2016-04-29 17:35:36 +0800

    add results progress bar

--- mainResults.py
+++ mainResults.py
@@ -5,13 +5,14 @@ from __init__ import confUser
 import pandas as pd
 import pickle as pkl
 import jushacore as mapper 
-from tools import to_d3js_graph
 import sys
 import os
+from tools import progressPrinter, to_d3js_graph
+
+pgPrinter = progressPrinter(-.2, .2)
 
 
-
-def core_wrapper(baseDir, data, filt, interval, overlap, fn, genJson= True):
+def core_wrapper(dataDir, data, filt, interval, overlap, fn, genJson= True):
     """
     class method of class coreWrapper. it recieves inputs required by jushacore as parameters
     and return the result if genJson==False, else write result to fn.
@@ -26,8 +27,12 @@ def core_wrapper(baseDir, data, filt, interval, overlap, fn, genJson= True):
     mapper_output = mapper.jushacore(data, filt, cover=cover,\
                              cutoff=None, cluster=cluster,\
                              metricpar=metricpar, verbose=False)
+    pgPrinter.printStep
+
     mapper.scale_graph(mapper_output, filt, cover=cover, weighting='inverse',\
                   exponent=1, verbose=False)
+    pgPrinter.printStep
+
     if mapper_output.stopFlag:
         print('{0} Stopped! Too many nodes or too long time'.format(fn))
     else:
@@ -37,35 +42,41 @@ def core_wrapper(baseDir, data, filt, interval, overlap, fn, genJson= True):
         with open('G.pkl', 'wb') as f:
             pkl.dump(mapper_output, f)
 
-        #resDir = os.path.join(baseDir, 'results')
-        to_d3js_graph(mapper_output, fn, baseDir, genJson)
+        
+        to_d3js_graph(mapper_output, fn, dataDir, genJson)
         print('Core ran finished! with: {0}'.format(fn))
+        pgPrinter.printStep
 
 
 if __name__=='__main__':
-	dataDir = sys.argv[1]
-	interval = sys.argv[2]
-	overlap = sys.argv[3]
-	filtKey = sys.argv[4]
-	print(type(interval))
+    dataDir = sys.argv[1]
+    interval = sys.argv[2]
+    overlap = sys.argv[3]
+    filtKey = sys.argv[4]
+    #filtKey = 'f2_107_170'
+    
 
-	dataDir = confUser['DATADIR']
-	dataTransDir = os.path.join(dataDir, 'transformed.csv')
-	data = pd.read_csv(dataTransDir).values
+    dataDir = confUser['DATADIR']
+    dataTransDir = os.path.join(dataDir, 'transformed.csv')
+    data = pd.read_csv(dataTransDir).values
+    pgPrinter.printStep
 
-	filtDir = os.path.join(dataDir, 'filterDic.pkl') 
-	with open(filtDir, 'rb') as f:
-		filtDic = pkl.load(f)
-	print(filtDic.keys())
-	filt = filtDic[filtKey]
+    filtDir = os.path.join(dataDir, 'filterDic.pkl') 
+    with open(filtDir, 'rb') as f:
+        filtDic = pkl.load(f)
+    print(filtDic.keys())
+    #filt = data[:,-2]
+    filt = filtDic[filtKey]
+    pgPrinter.printStep
 
-	fn = 'i'+interval+'o'+overlap+'_'+filtKey[3:]
+    fn = 'i'+str(interval)+'o'+str(overlap)+'_'+filtKey[3:]
 
-	core_wrapper(dataDir, data, filt, int(interval), int(overlap), fn)
+    core_wrapper(dataDir, data, filt, int(interval), int(overlap), fn)
 
 
 
-	
+    
+
 
 
 
