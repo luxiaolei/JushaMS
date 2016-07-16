8708a63cd3ec7b0a15a3d9a7c328239d37a49946
CCL <faller@users.noreply.github.com>
2016-07-16 11:14:06 +0800

    Update mainClean.py

--- mainClean.py
+++ mainClean.py
@@ -1,6 +1,5 @@
 """
 mainClean.py
-
 >>>>>>Functionalities:
 * Load data from 'data/session/' directory
     file name contains '客户画像', '金融资产', '产品交易, '基本信息'
@@ -14,22 +13,17 @@ mainClean.py
     Drop columns and rows which are justified as overfitting leakers. (neglect this process when in production)
     Transform 金融资产 table by dummy codes
     Drop duplicates
-
 * Inner Join dataframes
     Set '核心客户号' as indexs
     Inner join dataframes with last three columns as 'Y107','Y170','Y130'
-
 * Generates Dataframe for visual
     Parse '对应机构' file and join to '客户画像' table.
     Join 'Y107','Y170','Y130' to '客户画像' table
-
 * Print redirected to 'data/session/cleanLog.txt'
-
 >>>>>>Produce:
 * 'data/session/cleaned.csv' -Used for Qiangbao Visual
 * 'data/session/MasterDf.csv' 
 * 'data/session/CleanLog.txt'
-
 >>>>>>Run command from terminal:
 * python3 mainClean.py data/session
     
@@ -72,7 +66,7 @@ def getDfs(kw):
     dfs = []
     for k, fn in enumerate(paths):
         try:
-            df = pd.read_csv(fn, delimiter= '\t', encoding='gbk',
+            df = pd.read_csv(fn, delimiter= '|', encoding='gbk', 
                               error_bad_lines= False)
             #未知 can not be accepted by pandas
             df.replace(['未知', '其他', '?', ' '], np.nan, inplace=True)
@@ -96,9 +90,9 @@ def dfBuilder(kw, drop=True):
     dfs = getDfs(kw)
     dfFull = pd.concat(dfs, axis =0)
     if drop:
-        for uidName in ['核心客户号', 'v_khdm', '客户代码']:
+        for uidName in ['核心客户号', 'khdm', '客户代码']:
             try:
-                dfFull.drop_duplicates(subset=[uidName], inplace=True)
+                dfFull.drop_duplicates(subset=[uidname], inplace=True)    
             except Exception:
                 pass
     return dfFull
@@ -113,14 +107,14 @@ def intIndex(x):
 
     
 ############################################################################
-uimageCols = ['v_khdm', '性别', '年龄', '婚姻', '学历', '从属行业', '理财抗风险等级', '客户层级', '新老客户标记',
+uimageCols = ['客户代码', '性别', '年龄', '婚姻', '学历', '从属行业', '理财抗风险等级', '客户层级', '新老客户标记',
               '五级分类', '小微客户类型', '消费类资产产品', '纯消费性微贷标记', '纯质押贷款标记', '非储投资偏好', 
               '高金融资产标记', '购买大额他行理财标记', '大额消费标记', '信用卡高还款标记', '信用卡高端消费标记', 
               '优质行业标记', '高代发额客户标记', '潜在高端客户标记', '客户贡献度', '客户活跃度', '客户渠道偏好', '客户金融资产偏好']
 
-uinfoCols = ['v_khdm', '首次开户日期', '贵宾客户等级描述', '账户即时通签约标志',  '活期余额', '存款余额月日均', '存款余额年日均',
-         '存款占比', '理财占比', '金融资产余额', '金融资产余额月日均', '金融资产余额年日均',
-         '持有定期存款标志', '手机银行签约标志', '三方存管签约标志', '网银签约标志', '代发工资签约标志',
+uinfoCols = ['核心客户号', '首次开户日期', '贵宾客户等级描述', '账户即时通签约标志',  '活期余额', '存款余额月日均', '存款余额年日均',
+         '存款占比', '理财占比', '金融资产余额', '金融资产余额月日均', '金融资产余额年日均', '持有定期存款标志', '手机银行签约标志',
+         '持有定期存款标志', '手机银行签约标志', '三方存管签约标志', '网银签约标志', '代发工资签约标志', 
        '信用卡绑定还款签约标志', '按揭贷款标志',
        '当年购买理财标志', '钱生钱签约标志',  '资金归集签约标志', '乐收银签约标志',
        '近三个月柜面存款次数', '近三个月柜面存款金额', '近三个月柜面取款次数', '近三个月柜面取款金额', '近三个月柜面转账次数',
@@ -197,10 +191,10 @@ dftrade = dfBuilder('产品交易', drop=True)
 print('Trade Raw shape:', dftrade.shape)
 
 #index type convert
-dftrade['v_khdm'] = dftrade['v_khdm'].apply(intIndex)
+dftrade['核心客户号'] = dftrade['核心客户号'].apply(intIndex)
 
 #drop
-dftrade.drop(labels=['统计日期', 'CUST_NAME'], axis=1, inplace=True)
+dftrade.drop(labels=['统计日期', 'khdm', 'CUST_NAME'], axis=1, inplace=True)
 dftrade.dropna(inplace=True)
 print('Trade after clean shape:', dftrade.shape)
 print('<<<{0:.2f}>>>'.format(4.*n))
@@ -210,13 +204,13 @@ dfasset = dfBuilder('金融资产', drop=False)
 print('Asset Raw shape:', dfasset.shape)
 
 if not Consider0BalanceAsPositive:
-    uid107 = dfasset.ix[(dfasset['金融资产代码']==107)&(dfasset['金融资产余额']!=0), 'v_khdm'].drop_duplicates().values
-    uid170 = dfasset.ix[(dfasset['金融资产代码']==170)&(dfasset['金融资产余额']!=0), 'v_khdm'].drop_duplicates().values
-    uid130 = dfasset.ix[(dfasset['金融资产代码']==130)&(dfasset['金融资产余额']!=0), 'v_khdm'].drop_duplicates().values
+    uid107 = dfasset.ix[(dfasset['金融资产代码']==107)&(dfasset['金融资产余额']!=0), '核心客户号'].drop_duplicates().values
+    uid170 = dfasset.ix[(dfasset['金融资产代码']==170)&(dfasset['金融资产余额']!=0), '核心客户号'].drop_duplicates().values
+    uid130 = dfasset.ix[(dfasset['金融资产代码']==130)&(dfasset['金融资产余额']!=0), '核心客户号'].drop_duplicates().values
 else:
-    uid107 = dfasset.ix[dfasset['金融资产代码']==107, 'v_khdm'].drop_duplicates().values
-    uid170 = dfasset.ix[dfasset['金融资产代码']==170, 'v_khdm'].drop_duplicates().values
-    uid130 = dfasset.ix[dfasset['金融资产代码']==130, 'v_khdm'].drop_duplicates().values
+    uid107 = dfasset.ix[dfasset['金融资产代码']==107, '核心客户号'].drop_duplicates().values
+    uid170 = dfasset.ix[dfasset['金融资产代码']==170, '核心客户号'].drop_duplicates().values
+    uid130 = dfasset.ix[dfasset['金融资产代码']==130, '核心客户号'].drop_duplicates().values
 
 
 #select rows based on none-info leaking asset codes
@@ -229,10 +223,10 @@ dfs = []
 for code, df in uidGrouped:
     df['金融资产余额_'+str(code)] = df['金融资产余额'].values
     df['金融资产余额季日均_'+str(code)] = df['金融资产余额季日均'].values
-    newdf = df.ix[:, ['v_khdm', '金融资产余额_'+str(code), '金融资产余额季日均_'+str(code)]]
-    newdf.drop_duplicates(subset='v_khdm', inplace=True) #user can not hold one asset with multipul records
+    newdf = df.ix[:, ['核心客户号', '金融资产余额_'+str(code), '金融资产余额季日均_'+str(code)]]
+    newdf.drop_duplicates(subset='核心客户号', inplace=True) #user can not hold one asset with multipul records
    
-    newdf.set_index('v_khdm', drop=True, inplace=True)
+    newdf.set_index('核心客户号', drop=True, inplace=True)
     dfs.append(newdf)
 
 dfasset_dummy = dfs[0].join(dfs[1:])
@@ -241,19 +235,19 @@ print('Asset after clean shape:', dfasset_dummy.shape)
 print('<<<{0:.2f}>>>'.format(5.*n))
 sys.stdout.flush()
 ############################################################################
-dfuimage['Y107'] = dfuimage['v_khdm'].apply(lambda x: 1 if x in uid107 else 0)
-dfuimage['Y170'] = dfuimage['v_khdm'].apply(lambda x: 1 if x in uid170 else 0)
-dfuimage['Y130'] = dfuimage['v_khdm'].apply(lambda x: 1 if x in uid130 else 0)
+dfuimage['Y107'] = dfuimage['核心客户号'].apply(lambda x: 1 if x in uid107 else 0)
+dfuimage['Y170'] = dfuimage['核心客户号'].apply(lambda x: 1 if x in uid170 else 0)
+dfuimage['Y130'] = dfuimage['核心客户号'].apply(lambda x: 1 if x in uid130 else 0)
 
-dfuimage.set_index('v_khdm', drop= True, inplace= True)
-dftrade.set_index('v_khdm', drop= True, inplace= True)
-dfuinfo.set_index('v_khdm', drop= True, inplace= True)
+dfuimage.set_index('核心客户号', drop= True, inplace= True)
+dftrade.set_index('核心客户号', drop= True, inplace= True)
+dfuinfo.set_index('核心客户号', drop= True, inplace= True)
 
 dfXY = dfuimage.join([dftrade, dfuinfo, dfasset_dummy], how='inner')
 dfXY.fillna(0, inplace=True)
 
 #warning! dfXY.drop_duplicates(on all features).shape != (on uid only).shape
-dfXY = dfXY.reset_index().drop_duplicates(subset='v_khdm').set_index('v_khdm')
+dfXY = dfXY.reset_index().drop_duplicates(subset='核心客户号').set_index('核心客户号')
 print('Joint Master dataframe shape:', dfXY.shape)
 print('>>>>>'*20)
 
@@ -263,26 +257,26 @@ sys.stdout.flush()
 ############################################################################
 ##Generates Cleaned.csv
 
-# dforgs = dfBuilder('对应机构', drop=True)
-# dforgs.drop_duplicates(inplace=True)
-# dforgs.set_index('khdm', drop=True, inplace=True)
-# dforgs.rename(columns={'jgmc': '所属机构'}, inplace=True)
-#
+dforgs = dfBuilder('对应机构', drop=True)
+dforgs.drop_duplicates(inplace=True)
+dforgs.set_index('khdm', drop=True, inplace=True)
+dforgs.rename(columns={'jgmc': '所属机构'}, inplace=True)
+
 dfXYimage = dfXY[dfuimage.columns.values]
 print('XYimageshape:', dfXYimage.shape)
 
-dfCleaned = dfXYimage #.join(dforgs, how='left')
+dfCleaned = dfXYimage.join(dforgs, how='left')
 print('after join orgs, cleaned.csv shape:', dfCleaned.shape)
 
 #convert to raw uimage data
-dfuimageRaw.set_index('v_khdm', drop=True, inplace =True)
+dfuimageRaw.set_index('客户代码', drop=True, inplace =True)
 dfuimageRaw.drop_duplicates(inplace=True)
-dfCleaned = dfCleaned.ix[:, ['v_khdm','Y107','Y170','Y130']].join(dfuimageRaw, how='left')
+dfCleaned = dfCleaned.ix[:, -4:].join(dfuimageRaw, how='left')
 print('after join uimageRaw, cleaned.csv shape:', dfCleaned.shape)
 
 dfCleaned[['Y107', 'Y170', 'Y130']] = dfCleaned[['Y107', 'Y170', 'Y130']].astype(str)
-dfCleaned['v_khdm'] = dfCleaned.index.values
-dfCleaned.drop_duplicates(subset=['v_khdm'], inplace=True)
+dfCleaned['核心客户号'] = dfCleaned.index.values
+dfCleaned.drop_duplicates(subset=['核心客户号'], inplace=True)
 dfCleaned.rename(columns={'Y107':'主动负债', 'Y170': '保险', 'Y130': '基金'}, inplace=True)
 dfCleaned.replace(to_replace={'主动负债': {"0": '未持有', "1": '持有'},
                             '保险': {"0": '未持有', "1": '持有'},
